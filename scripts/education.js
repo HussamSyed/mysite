d4f4e4b9a5996f25509432bcb0790992589a9d08
Hussam Syed <hussamsyed8893@gmail.com>
2013-04-10 21:21:22 -0700

    general commit

---
+++ scripts/education.js
@@ -0,0 +1,13 @@
+education = {'1998':'Dar-ul-Arqam',
+			'2001':'Karachi International School', 
+			'2003':'School of Creative Science',
+			'2004':'Arab Unity School',
+			'2005':'Karachi International School',
+			'2008':'Suffah Saviour School',
+			'2010':'Beaconhouse Gulshan A Level',
+			'2012':'Mission College'};
+function list(year, school){
+	education = {year, school};
+	
+
+}
